6de647a4260c5f5bbe037de7d32ca7a3ef849d3d
Henry Stauch <henry.r.stauch.27@dartmouth.edu>
2025-04-17 17:56:35 -0400

    SA2 complete

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+function timeDisplay() {
+  $('#main').html(`You've been on this site for ${num} seconds.`);
+  num += 1;
+}
+
+setInterval(timeDisplay, 1000);

3d300d2508c10c755eadfcf0ff130ee3751fc178
Henry Stauch <henry.r.stauch.27@dartmouth.edu>
2025-05-02 21:23:53 -0400

    react-router added

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,52 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './style.scss';
-import Counter from './components/counter';
-import Controls from './components/controls';
+import {
+  BrowserRouter, Routes, Route, NavLink, useParams,
+} from 'react-router';
 
-function App() {
+function About(props) {
+  return <div> All there is to know about me </div>;
+}
+function Welcome(props) {
+  return <div>Welcome</div>;
+}
+
+function Nav(props) {
   return (
-    <div>
-      <div className="test">All the REACT are belong to us!</div>
-      <Counter />
-      <Controls />
-    </div>
+    <nav>
+      <ul>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+      </ul>
+    </nav>
+  );
+}
+
+function Test(props) {
+  const { id } = useParams();
+  return <div> ID: {id} </div>;
+}
+
+function FallBack(props) {
+  return <div>URL Not Found</div>;
+}
+
+function App(props) {
+  return (
+    <BrowserRouter>
+      <div>
+        <Nav />
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test/:id" element={<Test />} />
+          <Route path="*" element={<FallBack />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
